6824bc59838492b325a5ddc5e6c02920825caa2e
BanuelosMendezJordi <jordi.banuelos17@tectijuana.edu.mx>
2022-05-29 02:23:35 -0700

    Form and CSS update

--- scripts/practice.js
+++ scripts/practice.js
@@ -15,32 +15,33 @@ let student = {
     }
 }
 
-let products=[];
-let product1={
-    name:"Laptop",
-    description:'HP Laptop 14", core i7',
-    price: 200
-}
-let product2={
-    name:"Solid State Drive",
-    description:"Western Digital 256GB SSD",
-    price:35
-}
-let product3={
-    name:"Smartphone",
-    description: "Samsung Galaxy S7 ",
-    price: 400
-}
+// let products=[];
+// let product1={
+//     name:"Laptop",
+//     description:'HP Laptop 14", core i7',
+//     price: 200
+// }
 /*products.push();
 console.log(student.hobbies[1]);
 document.write(student.address);
 // Display the address on the console
 console.log(student.address);*/
-products = [product1, product2, product3];
-products.push(product1, product2, product3);
+// products = [product1, product2, product3];
+// products.push(product1, product2, product3);
 
 // console.log(products[2].name);
 
+function Product(n,d,p) {
+    this.name=n;
+    this.description=d;
+    this.price=p;
+}
+
+let Product1 = new Product ("Speaker","It doesn't have support for iOS", 20.50);
+let Product2 = new Product ("Laptop","HP",100);
+let Product3 = new Product ("Smart TV","Samsung 32 Smart TV", 150);
+let Product4 = new Product ("Smartphone", "Samsung Galaxy S8", 220);
+console.log(Product1, Product2, Product3, Product4);
 function displayNames(){
     for(let i=0;i<products.length;i=i+1)
     {
@@ -48,4 +49,4 @@ function displayNames(){
     }
 }
 
-displayNames();
+//displayNames();
--- scripts/register.js
+++ scripts/register.js
@@ -13,7 +13,7 @@ let salon={
     },
     phone:"555-5555",
     pets: [
-        {
+        /* {
             name: "Scooby",
             age: 50,
             gender: "Male",
@@ -36,37 +36,58 @@ let salon={
             age: 15,
             gender: "Male",
             breed: "Mixed Breed",
-            service: "fixing",
+            service: "vaccination",
             owner: "Vicky",
             phone: "222-2222"
-        }
-    ],
+        } */
+    ]
 }
+
+function Pet(n,a,g,b,s,o,p){
+    this.name=n;
+    this.age=a;
+    this.gender=g;
+    this.breed=b;
+    this.service=s;
+    this.owner=o;
+    this.phone=p;
+}
+
+/* Default Pets */
+let Scooby = new Pet ("Scooby", "50", "Male", "Dane", "Grooming", "Shaggy", "888-8888");
+let Dudley = new Pet ("Dudley", "10", "Male", "Mixed-Breed", "Grooming", "Peg", "777-7777");
+let Doidle = new Pet ("Doidle", "20", "Male", "Canaan", "Vaccines", "Vicky", "222-2222");
+salon.pets.push(Scooby, Dudley, Doidle);
+console.log(salon.pets[0],salon.pets[1],salon.pets[2]); 
+
+/* Clear all inputs in form */
+function clearForm(){
+    document.getElementById("txtName").value="";
+    document.getElementById("txtAge").value="";
+    document.getElementById("txtBreed").value="";
+    document.getElementById("txtOwner").value="";
+    document.getElementById("txtPhone").value="";
+}
+/* Registering pets */
+function register(){
+    console.log("Registering...");
+    //Get the info from the inputs ***getElementbyId().value
+    let petName = document.getElementById("txtName").value;
+    let petAge = document.getElementById("txtAge").value;
+    let petGender = document.getElementById("selGender").value;
+    let petBreed = document.getElementById("txtBreed").value;
+    let petService = document.getElementById("selService").value;
+    let petOwner = document.getElementById("txtOwner").value;
+    let petPhone = document.getElementById("txtPhone").value;
+    //Create the object using the constructor
+    let newPet = new Pet (petName, petAge, petGender, petBreed, petService, petOwner, petPhone);
+    //Push the object into the array
+    salon.pets.push(newPet);
+    //Display on console
+    console.log(newPet);
+    clearForm();
+}
+
 //console.log(salon.pets[i]); //Displays Scooby
 
-function displayPetsNameConsole() {
-    for(let i=0; i<salon.pets.length;i=i+1)
-    {
-        console.log(salon.pets[i].name);
-    }
-}
-function displayPetsName() {
-    for(let i=0; i<salon.pets.length;i=i+1)
-    {
-        let tmp=`
-        <p>Pet name: ${salon.pets[i].name}</p>`; document.getElementById("pets").innerHTML+=tmp;
-    }
-}
-function displaySalonInfo() {
-    let tmp=`
-    <p>Welcome to ${salon.name}</p>
-    <p>Location: ${salon.address.street}, ${salon.address.zip}, ${salon.address.city}. ${salon.address.number}</p>
-    <p>Opening time: ${salon.hours.open}</p>
-    <p>Closing time: ${salon.hours.close}</p>`;
-    document.getElementById("info").innerHTML=tmp;
-}
-
 alert(`There are ${salon.pets.length} pets registered so far`);
-displayPetsNameConsole();
-displayPetsName();
-displaySalonInfo();
